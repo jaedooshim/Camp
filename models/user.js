c66728379d6610c1b31db2f00cdbb9d26f9a4e28
shimjaedoo <woen1637@naver.com>
2023-06-26 00:11:34 +0900

    sequelize,migration,cascade prac

---
+++ models/user.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Post, Comment }) {
+      // define association here
+      this.hasMany(Post, { foreignKey: "userId", as: "posts" });
+      this.hasMany(Comment, { foreignKey: "userId", as: "comments" });
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: "users",
+      modelName: "User",
+    }
+  );
+  return User;
+};
